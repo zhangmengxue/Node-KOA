a00ec5c05670df8398007f1cfebf2ce268cd5eb0
艺璇 <mengxue.zmx@alibaba-inc.com>
2015-07-02 11:14:47 +0800

    socket chat

---
+++ websocket/chat-application/index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  //监听客户端的消息
+  socket.on('chat message', function(msg){
+  	//用于将消息发送给每个人，包括发送者
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
