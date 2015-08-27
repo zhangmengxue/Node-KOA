a2cae61196838ed393ae33a1e99185e058ec2b08
yixuan <mengxue.zmx@alibaba-inc.com>
2015-08-27 14:27:17 +0800

    fix

--- restfulAPI/server.js
+++ restfulAPI/server.js
@@ -11,22 +11,44 @@ var db = monk('localhost:27017/restdata');
 app.use(bodyParser.urlencoded({ extended : true}));
 app.use(bodyParser.json());
 
-var port = 8080;
+app.use(express.static(__dirname+'/public'));
 
-var router = express.Router();
+var collectorder = db.get('orderlist');
+var collectuser = db.get('userlist');
 
-router.get('/',function(req,res){
-  //res.json({message:'hi,its my api!'});
-  var collection = db.get('orderlist');
-  collection.find({},function(e,docs){
+
+app.get('/orders',function(req,res){
+  collectorder.find({},{limit:20},function(err,orders){
+    res.json(orders);
+  });
+});
+app.get('/users',function(req,res){
+  collectuser.find({},{},function(err,users){
+    res.json(users);
+  });
+});
+
+app.get('/users/:name',function(req,res){
+  //var collection = db.get(req.params.name);
+  //console.log(req.params.name);
+  collectuser.find({name:req.params.name},{},function(err,docs){
     res.json(docs);
   });
 });
 
+//客户端通过请求adduser接口post数据进入数据库
+// app.post('/adduser',function(req,res){
+//   var db = req.db;
+//   collectuser.insert(req.body,function(err,result){
+//     res.send(
+//       (err == null) ? {msg:''}:{msg:err}
+//       );
+//   });
+// });
 
-app.use('/api/',router);
 
-app.listen(port,function(){
-console.log('PORT start....');
 
-});
+
+app.listen(3000);
+
+
